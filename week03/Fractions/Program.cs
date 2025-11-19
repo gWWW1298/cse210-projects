d975a97395d5c57316b929f1cdd1a96458c355ff
gWWW1298 <yandriambolatahiana@byupathway.edu>
2025-11-19 14:21:48 +0300

    w3_1st

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction x1 = new Fraction();
+        Fraction x2 = new Fraction(5);
+        Fraction x3 = new Fraction(3,4);
+
+        // Console.WriteLine($"top x1={x1.GetTop()}");
+        // Console.WriteLine($"bottom x1={x1.GetBottom()}");
+        
+        // x1.SetTop(2);
+        // x1.SetBottom(3);
+        // Console.WriteLine($"top x1={x1.GetTop()}");
+        // Console.WriteLine($"bottom x1={x1.GetBottom()}");
+
+        Console.WriteLine($"{x1.GetFractionString()}");
+        Console.WriteLine($"{x1.GetDecimalValue()}");
+
+        Console.WriteLine($"{x2.GetFractionString()}");
+        Console.WriteLine($"{x2.GetDecimalValue()}");
+
+        Console.WriteLine($"{x3.GetFractionString()}");
+        Console.WriteLine($"{x3.GetDecimalValue()}");
+
     }
 }
