634f9594f78f25750b1947b0b228a3af5e1063ed
gWWW1298 <yandriambolatahiana@byupathway.edu>
2025-12-08 17:42:17 +0300

    w06 learningActivity

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,17 @@
+
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius):base(color)
+    {
+        SetColor(color);
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return 3.14 * _radius * _radius;
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Console.WriteLine("Hello World! This is the Shapes Project.");
+        
+        // Create a list of Shape
+        List<Shape> xy = new List<Shape>();
+
+        // Create different kinds of shapes and add them to the same list
+        xy.Add(new Square("red", 2));
+        xy.Add(new Rectangle("green", 3,9));
+        xy.Add(new Circle("yellow", 3));
+
+        // Get a custom calculation for each one
+        foreach(Shape elt in xy)
+        {
+        string color = elt.GetColor();
+        double area = elt.GetArea();
+        Console.WriteLine(color+" "+area);
+        }
     }
 }
