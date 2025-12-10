33cd31732a5a2f778e5dffb5a7bdce3909c521b3
gWWW1298 <yandriambolatahiana@byupathway.edu>
2025-12-10 19:45:07 +0300

    W7 tracking exercise

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Nov 2022", 30, 3.0));
+        activities.Add(new Cycling("03 Nov 2022", 40, 12.0));
+        activities.Add(new Swimming("03 Nov 2022", 25, 20));
+
+        foreach (Activity a in activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,31 @@
+
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running(string date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public double GetStoredDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
